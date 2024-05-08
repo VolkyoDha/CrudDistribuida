d2b86f1fd803ca9f9b30f1e9cf463d8193a5df93
Carlos <clobodiaz@outlook.com>
2024-05-07 19:28:36 -0500

    chore: Add DB_URI to .env and connect to MongoDB in main.js

--- main.js
+++ main.js
@@ -2,38 +2,45 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: "complex-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Database connection
 mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Database"));
 
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use(session({
-    secret: 'KEYSECRET',
-    resave: false,
-    saveUninitialized: true
-    })
-);
-
-app.use((req, res, next) => {
-    res.locals.message = req.session.message;
-    delete req.session.message;
-    next();
-});
-
+// Set template engine
 app.set("view engine", "ejs");
 
-app.use("", require("./routes/routes"));
+// Routes
+const routes = require("./routes/routes");
+app.use("/", routes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+//logic for image icon
+app.use(express.static("uploads"));
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server started at https://localhost:${PORT}`);
+  console.log(`App is listening on http://localhost:${PORT}`);
 });
